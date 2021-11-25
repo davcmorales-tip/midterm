c9f8ed0fc2bec26a30f864459f150c32c1219978
davcmorales-tip <qdavcmorales@tip.edu.ph>
2021-11-25 00:34:07 +0000

    Added New Files

---
+++ tempdir/midterm.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+
+design = Flask(__name__)
+
+@design.route("/")
+def main():
+    return render_template("login.html")
+
+@design.route("/register")
+def register():
+    return render_template("register.html")
+
+@design.route("/login")
+def login():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    design.run(host="0.0.0.0", port=5000)
